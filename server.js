cfe45f13e9002f8a80b6f36876e7397c8ca89e03
CKPRASHANTH07 <103945546+CKPRASHANTH07@users.noreply.github.com>
2024-07-02 19:57:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,31 @@
-const express = require('express');
-const cors = require('cors');
-const postsRouter = require('./routes/posts');
+const express = require("express");
+const cors = require("cors");
+const postsRouter = require("./routes/posts");
 
 const app = express();
 const PORT = process.env.PORT || 8000;
-
+const path = require("path");
 app.use(cors());
 app.use(express.json({ extended: false }));
 
-app.use('/', postsRouter);
+const __dirname1 = path.resolve();
+
+if (true) {
+	app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+	app.get("*", (req, res) =>
+		res.sendFile(path.resolve(__dirname1, "Frontend", "build", "index.html")),
+	);
+	// app.get("/", (req, res) => {
+	//   res.send("API is running..");
+	// });
+} else {
+	app.get("/", (req, res) => {
+		res.send("API is running..");
+	});
+}
+app.use("/", postsRouter);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+	console.log(`Server is running on port ${PORT}`);
 });
